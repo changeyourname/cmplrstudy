c10df5783053bc20fd3273111e683371360bd379
bepshatsky <bepshatsky@gmail.com>
2011-11-27 10:11:15 +0000

    Added register classes APR and FLAGS(P_FLAG and S_FLAG)

--- tesys/build.pl
+++ tesys/build.pl
@@ -6,14 +6,17 @@ my $LLVM_Root = "./..";
 my $LogPath = "build.report.log";
 my $Options = "Echo='\@perl \$(LLVM_SRC_ROOT)/tesys/echo.pl \$(LLVM_SRC_ROOT)/tesys/build.report.log' ";
 
-my ($cmd, $errcode, $help, $tool, $clean, $verbose);
+my ($cmd, $errcode, $help, $tool, $tblgen, $clean, $verbose);
 my $usage = "Usage: build.pl --clean --tool <list>\
 \tclean\t- if tool isn't set, then clean all, overwise clean specified tools\
-\ttool\t- set tool list for building\n";
+\ttool\t- set tool list for building\
+\ttblgen\t- generate C++ code from *.td-files. Use reg, instr or callingconv\
+\thelp\t- print this message\n";
 my %options = (
 		'help|h|?' => \$help,
 		'clean' => \$clean,
 		'tool=s' => \$tool,
+		'tblgen=s' => \$tblgen,
 		'verbose|v' => \$verbose);
 
 GetOptions(%options);
@@ -91,6 +94,70 @@ if ($clean) {
 	print "Cleaning Successfully completed!\n";
 	exit 0;
 }
+elsif ($tblgen) {
+	chdir "$LLVM_Root/lib/Target/MDSP";
+
+	$tblgen = "$LLVM_Root/Debug/bin/tblgen MDSP.td -I=\"$LLVM_Root/include\"";
+
+	@tdlist = split(/,/, $tblgen);
+
+	foreach $td (@tdlist) {
+		if ($td == "reg") {
+			$cmd = "$tblgen -gen-register-desc-header > MDSPGenRegisterInfo.h.inc";
+			system $cmd;
+
+			if ($?) {
+				print "ERROR: Cannot generate MDSPGenRegisterInfo.h.inc\n";
+				next;
+			}
+
+			$cmd = "$tblgen -gen-register-desc > MDSPGenRegisterInfo.inc";
+			system $cmd;
+
+			if ($?) {
+				print "ERROR: Cannot generate MDSPGenRegisterInfo.inc\n";
+			}
+
+			$cmd = "$tblgen -gen-register-enums > MDSPGenRegisterNames.inc";
+			system $cmd;
+
+			if ($?) {
+				print "ERROR: Cannot generate MDSPGenRegisterNames.inc\n";
+			}
+		}
+		elsif ($td == "instr") {
+			$cmd = "$tblgen -gen-instr-desc > MDSPGenInstrInfo.inc";
+			system $cmd;
+
+			if ($?) {
+				print "ERROR: Cannot generate MDSPGenInstrInfo.inc\n";
+				next;
+			}
+
+			$cmd = "$tblgen -gen-instr-enums > MDSPGenInstrNames.inc";
+			system $cmd;
+
+			if ($?) {
+				print "ERROR: Cannot generate MDSPGenInstrNames.inc\n";
+			}
+		}
+		elsif ($td == "callingconv") {
+			$cmd = "$tblgen -gen-callingconv > MDSPGenCallingConv.inc";
+			system $cmd;
+
+			if ($?) {
+				print "ERROR: Cannot generate MDSPGenCallingConv.inc\n";
+				next;
+			}
+		}
+		else {
+			print "Unrecognize option. Skipping...\n";
+			next;
+		}
+	}
+
+	chdir $LLVM_Root;
+}
 elsif ($tool) {
 	print "Tool\t\t$tool\n";
 	
@@ -103,33 +170,32 @@ else {
 		print "ERROR[$?]: Cannot configure llvm!\n";
 		exit 1;
 	}
-}
 
-$cmd = "make -j 2 -f $LLVM_Root/Makefile $Options";
+	$cmd = "make -j 2 -f $LLVM_Root/Makefile $Options";
 
-open BuildReport, $LogPath;
-print BuildReport "Date:\t".`date`;
-print BuildReport "Options:\t$Options\n";
-print BuildReport "Command:\t$cmd\n";
-close BuildReport;
-
-chdir $LLVM_Root;
-system $cmd;
-
-$errcode = $?;
-
-print "ErrCode: $?\n";
-
-if ($errcode) {
 	open BuildReport, $LogPath;
-	print BuildReport "ERROR: Building tools was failed with error code $errcode\n";
+	print BuildReport "Date:\t".`date`;
+	print BuildReport "Options:\t$Options\n";
+	print BuildReport "Command:\t$cmd\n";
 	close BuildReport;
 
-	print "ERROR: Building Tools!\n";
-} else {
-	print "Building successfully completed!\n";
-}
+	chdir $LLVM_Root;
+	system $cmd;
 
-exit $errcode;
+	$errcode = $?;
+
+	print "ErrCode: $?\n";
+
+	if ($errcode) {
+		open BuildReport, $LogPath;
+		print BuildReport "ERROR: Building tools was failed with error code $errcode\n";
+		close BuildReport;
+
+		print "ERROR: Building Tools!\n";
+		exit $errcode;
+	} else {
+		print "Building successfully completed!\n";
+	}
+}
 
 
